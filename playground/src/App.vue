a76dc7314e0335060fb9c12c8aecf87882ce000e
XFeng <wxfengg@qq.com>
2026-01-30 18:18:21 +0800

    feat: add playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,14 +3,73 @@ import { ref } from "vue"
 import { Modal } from "../../src/index.ts"
 
 const visible1 = ref(false)
+const visible2 = ref(false)
+const visible3 = ref(false)
+const visible4 = ref(false)
+const visible5 = ref(false)
+const visible6 = ref(false)
+const visible7 = ref(false)
+const visible8 = ref(false)
+const visible9 = ref(false)
 </script>
 
 <template>
   <div class="app">
     <button class="button" @click="visible1 = true">基础弹窗</button>
+    <button class="button" @click="visible2 = true">无遮罩</button>
+    <button class="button" @click="visible3 = true">拖拽</button>
+    <button class="button" @click="visible4 = true">拖拽-关闭复位</button>
+    <button class="button" @click="visible5 = true">关闭方式</button>
+    <button class="button" @click="visible6 = true">center</button>
+    <div style="cursor: pointer" @click="visible7 = true">文字触发</div>
+    <button class="button" style="position: fixed; left: 10px; top: 10px" @click="visible8 = true">左上的按钮</button>
+    <button class="button" style="position: fixed; right: 10px; bottom: 10px" @click="visible9 = true">
+      右下的按钮
+    </button>
 
     <Modal v-model="visible1" title="基础弹窗">
-      <div>基础的弹窗</div>
+      <h4>基础的弹窗演示</h4>
+      <div>这是一个基础的弹窗，它很普通</div>
+    </Modal>
+
+    <Modal v-model="visible2" title="无遮罩弹窗" width="500" :overlay="false" :lock-scroll="false">
+      <h4>无遮罩弹窗演示</h4>
+      <div>没有遮罩，像是一个悬浮窗。遮罩相关的API不生效</div>
+    </Modal>
+
+    <Modal v-model="visible3" title="拖拽弹窗" draggable>
+      <h4>拖拽弹窗演示</h4>
+      <div>可以在头部进行拖拽，如果没有 header 那么拖拽也无法使用</div>
+    </Modal>
+
+    <Modal v-model="visible4" title="拖拽弹窗" draggable drag-close-reset>
+      <h4>拖拽弹窗演示</h4>
+      <div>设置 dragCloseReset 关闭弹窗之后回到初始位置，否则保留上次拖拽的位置</div>
+    </Modal>
+
+    <Modal v-model="visible5" title="关闭方式" :close-on-overlay="false">
+      <h4>关闭方式演示</h4>
+      <div>默认可以点击 x / 遮罩 / Esc 关闭，遮罩和 Esc 可以配置为 false</div>
+    </Modal>
+
+    <Modal v-model="visible6" title="center" :center="false">
+      <div>默认 center 为 true，弹窗会页面垂直居中，如果设置 top(默认15vh) 会覆盖 center</div>
+      <div>如果 center 为 false 并且没有设置 top 默认也会设置 top(默认15vh)</div>
+      <div style="height: 1800px; background-color: plum">占位</div>
+    </Modal>
+
+    <Modal v-model="visible7" title="top" width="300" top="10vh">
+      <div>如果 center 为 false 并且没有设置 top 默认也会设置 top(默认15vh)</div>
+    </Modal>
+
+    <Modal v-model="visible8" title="左上角的按钮" width="800">
+      <div>左上角的按钮</div>
+      <div style="height: 500px; background-color: skyblue">占位</div>
+    </Modal>
+
+    <Modal v-model="visible9" title="右下角的按钮" width="500" :center="false">
+      <div>右下角的按钮</div>
+      <div style="height: 500px; background-color: darkgray">占位</div>
     </Modal>
   </div>
 </template>
@@ -20,9 +79,13 @@ body {
   margin: 0;
   padding: 0;
   font-family: system-ui, sans-serif;
+  line-height: 1.5;
+  background-color: #edf2fa;
+  overflow-x: hidden;
 }
 
 .app {
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
