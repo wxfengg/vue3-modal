f1d748ec7eeea34229b77334fdf383004b5ddcfe
XFeng <wxfengg@qq.com>
2026-01-31 02:03:58 +0800

    reactor: replace the bundler with tsdown and remove unnecessary dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import { resolve } from "path"
 
 export default defineConfig({
   // 项目根目录（index.html 文件所在的位置）
   root: "./playground",
   plugins: [vue()],
-  server: {
-    host: true,
-    port: 5173,
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "Vue3Modal",
-      formats: ["es", "cjs"],
-      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: { vue: "vue" },
-      },
-    },
-  },
 })
